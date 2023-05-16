9e2e981055c0588f1fbe045bf2e12a5c6bf22eeb
Tomás Neves <2201747@my.ipleiria.pt>
2023-05-17 00:20:23 +0100

    login view

---
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/LoginActivity.java
@@ -0,0 +1,14 @@
+package ipl.estg.happyguest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/MainActivity.java
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/MainActivity.java
@@ -2,10 +2,13 @@ package ipl.estg.happyguest.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import java.util.Objects;
 
+import ipl.estg.happyguest.LoginActivity;
 import ipl.estg.happyguest.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2500);
     }
 }
