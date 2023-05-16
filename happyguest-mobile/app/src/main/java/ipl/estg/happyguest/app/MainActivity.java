fa836e8947251998ef5c3ee0bde3cfed179969ad
Diogo Mendes <diiogomendes21@hotmail.com>
2023-05-16 20:12:55 +0100

    Add Main Activity

---
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/MainActivity.java
@@ -0,0 +1,16 @@
+package ipl.estg.happyguest.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ipl.estg.happyguest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
