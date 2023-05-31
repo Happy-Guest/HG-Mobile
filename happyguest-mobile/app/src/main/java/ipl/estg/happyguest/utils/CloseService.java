7bcca4ca22143245ee06ba8b1bd053edb0cbfd93
Diogo Mendes <diiogomendes21@hotmail.com>
2023-05-31 04:53:01 +0100

    Bug Fix

--- happyguest-mobile/app/src/main/java/ipl/estg/happyguest/utils/CloseService.java
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/utils/CloseService.java
@@ -3,23 +3,18 @@ package ipl.estg.happyguest.utils;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.widget.Toast;
+import android.os.StrictMode;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import ipl.estg.happyguest.R;
+import java.io.IOException;
+
 import ipl.estg.happyguest.utils.api.APIClient;
 import ipl.estg.happyguest.utils.api.APIRoutes;
 import ipl.estg.happyguest.utils.api.responses.MessageResponse;
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class CloseService extends Service {
-
-    private Token token;
-    private APIRoutes api;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -34,30 +29,18 @@ public class CloseService extends Service {
 
     @Override
     public void onTaskRemoved(Intent rootIntent) {
-        token = new Token(this);
-        api = APIClient.getClient().create(APIRoutes.class);
-        APIClient.setToken(token.getToken());
+        Token token = new Token(this);
+        APIRoutes api = APIClient.getClient().create(APIRoutes.class);
         if (!token.getRemember()) {
-            Toast.makeText(this, getString(R.string.session_terminated), Toast.LENGTH_SHORT).show();
-            logout();
+            token.clearToken();
+            try {
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                Call<MessageResponse> call = api.logout();
+                call.execute();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-    }
-
-    private void logout() {
-        Call<MessageResponse> call = api.logout();
-        call.enqueue(new Callback<MessageResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<MessageResponse> call, @NonNull Response<MessageResponse> response) {
-                if (response.isSuccessful()) {
-                    token.clearToken();
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<MessageResponse> call, @NonNull Throwable t) {
-                Toast.makeText(CloseService.this, "Erro: " + t.getMessage(), Toast.LENGTH_LONG).show();
-                call.cancel();
-            }
-        });
     }
 }
