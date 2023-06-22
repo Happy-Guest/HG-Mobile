51249fe4713cd435b118af606e7522b712f8a8a0
Diogo Mendes <diiogomendes21@hotmail.com>
2023-06-22 21:12:12 +0100

    Multiple Files

--- happyguest-mobile/app/src/main/java/ipl/estg/happyguest/utils/api/requests/ComplaintRequest.java
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/utils/api/requests/ComplaintRequest.java
@@ -4,32 +4,33 @@ import androidx.annotation.Nullable;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class ComplaintRequest {
     @Nullable
     @SerializedName("user_id")
     private final Integer user_id;
-
     @SerializedName("title")
     private final String title;
-
     @SerializedName("local")
     private final String local;
-
     @SerializedName("status")
     private final String status;
-
     @SerializedName("comment")
     private final String comment;
-
     @SerializedName("date")
     private final String date;
+    @Nullable
+    @SerializedName("filesBase64")
+    private final List<String> filesBase64;
 
-    public ComplaintRequest(@Nullable Integer user_id, String title, String local, String status, String comment, String date) {
+    public ComplaintRequest(@Nullable Integer user_id, String title, String local, String status, String comment, String date, @Nullable List<String> filesBase64) {
         this.user_id = user_id;
         this.title = title;
         this.local = local;
         this.status = status;
         this.comment = comment;
         this.date = date;
+        this.filesBase64 = filesBase64;
     }
 }
