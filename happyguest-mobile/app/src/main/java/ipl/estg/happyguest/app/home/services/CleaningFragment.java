0bfc88fa568432ab06ab0f05c612d1b1aea479ba
Diogo Mendes <diiogomendes21@hotmail.com>
2023-07-06 16:25:05 +0100

    Clening & Service Details

--- happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/home/services/CleaningFragment.java
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/home/services/CleaningFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -41,6 +42,26 @@ public class CleaningFragment extends Fragment {
         return binding.getRoot();
     }
 
+    private String formatSchedule(String schedule) {
+        // String is 9-14-15-21 and we want 9h-14h and 15h-21h
+        String[] scheduleArray = schedule.split("-");
+        StringBuilder scheduleString = new StringBuilder();
+        for (int i = 0; i < scheduleArray.length; i++) {
+            if (i % 2 == 0) {
+                scheduleString.append(scheduleArray[i]).append("h-");
+            } else {
+                scheduleString.append(scheduleArray[i]).append("h");
+                if (i != scheduleArray.length - 1) {
+                    scheduleString.append(" e ");
+                }
+            }
+        }
+        if (scheduleString.toString().isEmpty()) {
+            return schedule;
+        }
+        return scheduleString.toString();
+    }
+
     private void getServiceAttempt() {
         Call<ServiceResponse> call = api.getService(1L);
         call.enqueue(new Callback<ServiceResponse>() {
@@ -51,25 +72,47 @@ public class CleaningFragment extends Fragment {
                 if (response.isSuccessful() && response.body() != null) {
                     // Get Service and populate fields
                     Service service = response.body().getService();
-                    String schedule = getString(R.string.services_schedule) + " " + Objects.requireNonNull(service).getSchedule();
-                    binding.txtServiceSchedule.setText(schedule);
+                    String schedule = getString(R.string.services_schedule) + " " + formatSchedule(Objects.requireNonNull(service).getSchedule());
+                    binding.cleaningService.txtServiceSchedule.setText(schedule);
                     if (service.getEmail() != null) {
-                        binding.txtServiceEmail.setVisibility(View.VISIBLE);
-                        binding.txtEmail.setVisibility(View.VISIBLE);
-                        binding.txtServiceEmail.setText(service.getEmail());
+                        binding.cleaningService.serviceEmail.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_in_fast));
+                        binding.cleaningService.serviceEmail.setVisibility(View.VISIBLE);
+                        binding.cleaningService.txtServiceEmail.setText(service.getEmail());
                     } else {
-                        binding.txtServiceEmail.setVisibility(View.GONE);
-                        binding.txtEmail.setVisibility(View.GONE);
+                        binding.cleaningService.serviceEmail.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_out_fast));
+                        binding.cleaningService.serviceEmail.setVisibility(View.GONE);
                     }
                     if (service.getPhone() != null) {
-                        binding.txtServicePhone.setVisibility(View.VISIBLE);
-                        binding.txtPhone.setVisibility(View.VISIBLE);
-                        binding.txtServicePhone.setText(String.format(Locale.getDefault(), "%d", service.getPhone()));
+                        binding.cleaningService.servicePhone.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_in_fast));
+                        binding.cleaningService.servicePhone.setVisibility(View.VISIBLE);
+                        binding.cleaningService.txtServicePhone.setText(String.format(Locale.getDefault(), "%d", service.getPhone()));
                     } else {
-                        binding.txtServicePhone.setVisibility(View.GONE);
-                        binding.txtPhone.setVisibility(View.GONE);
+                        binding.cleaningService.servicePhone.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_out_fast));
+                        binding.cleaningService.servicePhone.setVisibility(View.GONE);
                     }
-                    binding.txtDescription.setText(service.getDescription());
+                    if (service.getOccupation() != null) {
+                        binding.cleaningService.serviceOccupation.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_in_fast));
+                        binding.cleaningService.serviceOccupation.setVisibility(View.VISIBLE);
+                        binding.cleaningService.txtServiceOccupation.setText(String.format(Locale.getDefault(), "%d", service.getOccupation()));
+                    } else {
+                        binding.cleaningService.serviceOccupation.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_out_fast));
+                        binding.cleaningService.serviceOccupation.setVisibility(View.GONE);
+                    }
+                    if (service.getLocation() != null) {
+                        binding.cleaningService.serviceLocation.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_in_fast));
+                        binding.cleaningService.serviceLocation.setVisibility(View.VISIBLE);
+                        binding.cleaningService.txtServiceLocation.setText(service.getLocation());
+                    } else {
+                        binding.cleaningService.serviceLocation.setAnimation(AnimationUtils.loadAnimation(binding.getRoot().getContext(), R.anim.fade_out_fast));
+                        binding.cleaningService.serviceLocation.setVisibility(View.GONE);
+                    }
+                    // Get language code and set description
+                    String languageCode = Locale.getDefault().getLanguage();
+                    if (languageCode.equals("pt")) {
+                        binding.cleaningService.txtDescription.setText(service.getDescription());
+                    } else {
+                        binding.cleaningService.txtDescription.setText(service.getDescriptionEN());
+                    }
                 } else {
                     Toast.makeText(binding.getRoot().getContext(), getString(R.string.api_error), Toast.LENGTH_SHORT).show();
                     Log.i("GetService Error: ", response.message());
