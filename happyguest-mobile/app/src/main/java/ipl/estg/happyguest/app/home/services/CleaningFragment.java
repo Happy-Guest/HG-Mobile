00c4fb74a41d80141e8052782ef36d48c7617c1e
Diogo Mendes <diiogomendes21@hotmail.com>
2023-07-07 01:53:08 +0100

    Schedule Regex

--- happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/home/services/CleaningFragment.java
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/home/services/CleaningFragment.java
@@ -97,7 +97,7 @@ public class CleaningFragment extends Fragment {
 
     private String formatSchedule(String schedule) {
         // Schedule format is 9:00-14:00-15:00-21:00 or 9:00-23:00
-        // We want 9:00h-14:00h and 15:00h-21:00h
+        // Split the schedule into start and end times
         String[] scheduleArray = schedule.split("-");
         StringBuilder scheduleString = new StringBuilder();
         for (int i = 0; i < scheduleArray.length; i++) {
@@ -117,18 +117,50 @@ public class CleaningFragment extends Fragment {
     }
 
     private void populateScheduleSpinner(String schedule) {
-        // Schedule format is 9:00-14:00-15:00-21:00 or 9:00-23:00
-        // We want 15 minute intervals respecting the schedule
-
-        // TODO: Generate schedule times in 15-minute intervals for the specified number of hours (schedule) HH:mm
-
+        // Schedule format is 9:00-14:00-15:00-21:00 or 9:30-23:00
         List<String> availableDates = new ArrayList<>();
+        List<String> scheduleDates = new ArrayList<>();
+
+        // Split the schedule into start and end times
+        String[] scheduleArray = schedule.split("-");
+        for (int i = 0; i < scheduleArray.length; i += 2) {
+            // Get the start and end times
+            String[] scheduleStartTime = scheduleArray[i].split(":");
+            int hour = Integer.parseInt(scheduleStartTime[0]);
+            int minute = Integer.parseInt(scheduleStartTime[1]);
+            String[] scheduleEndTime = scheduleArray[i + 1].split(":");
+            int endHour = Integer.parseInt(scheduleEndTime[0]);
+            int endMinute = Integer.parseInt(scheduleEndTime[1]);
+
+            // Get the start time
+            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Lisbon"));
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+            calendar.set(Calendar.SECOND, 0);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
+            dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Lisbon"));
+            String startTime = dateFormat.format(calendar.getTime());
+            scheduleDates.add(startTime);
+
+            // Get all the dates between the start and end times in 15-minute intervals
+            for (; hour <= endHour; hour++) {
+                for (; minute < 60; minute += 15) {
+                    if (hour == endHour && minute > endMinute) {
+                        break;
+                    }
+                    calendar.set(Calendar.HOUR_OF_DAY, hour);
+                    calendar.set(Calendar.MINUTE, minute);
+                    String date = dateFormat.format(calendar.getTime());
+                    scheduleDates.add(date);
+                }
+                minute = 0;
+            }
+        }
 
         // Set the start time to the next nearest 15-minute mark
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Lisbon"));
         calendar.add(Calendar.MINUTE, (15 - calendar.get(Calendar.MINUTE) % 15) % 15);
         calendar.set(Calendar.SECOND, 0);
-
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
         dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Lisbon"));
         String startTime = dateFormat.format(calendar.getTime());
@@ -138,10 +170,10 @@ public class CleaningFragment extends Fragment {
         for (int i = 0; i < 24 * 4; i++) {
             calendar.add(Calendar.MINUTE, 15);
             String date = dateFormat.format(calendar.getTime());
-
-            // TODO: If date is in the generated schedule times, add it to the spinner
-            // availableDates.add(date);
-
+            // Check if date is in the schedule
+            if (scheduleDates.contains(date)) {
+                availableDates.add(date);
+            }
             if (calendar.get(Calendar.DATE) - 1 > Calendar.getInstance().get(Calendar.DATE)) {
                 break; // Stop after 24 hours
             }
