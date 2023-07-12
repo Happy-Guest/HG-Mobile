85f882c176d109d7e3d10488d38c9f4ac82597d8
Tomás Neves <2201747@my.ipleiria.pt>
2023-07-12 23:45:02 +0100

    list reserves

---
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/app/home/reserve/ReservesAdapter.java
@@ -0,0 +1,114 @@
+package ipl.estg.happyguest.app.home.reserve;
+
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import ipl.estg.happyguest.R;
+import ipl.estg.happyguest.app.home.HomeActivity;
+import ipl.estg.happyguest.utils.models.Order;
+import ipl.estg.happyguest.utils.models.Reserve;
+
+public class ReservesAdapter extends RecyclerView.Adapter<ReservesAdapter.ViewHolder> {
+
+    private final ArrayList<Reserve> reservesList;
+    private final Context context;
+
+    public ReservesAdapter(ArrayList<Reserve> reservesList, Context context) {
+        this.reservesList = reservesList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.reserve_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get Services
+        Reserve reserve = reservesList.get(position);
+
+        // Set Texts
+        switch (reserve.getService().type) {
+            case 'R':
+                holder.iconService.setImageResource(R.drawable.restaurant_icon);
+                break;
+            case 'O':
+                holder.iconService.setImageResource(R.drawable.reserve_icon);
+                break;
+        }
+        String nameService = Locale.getDefault().getLanguage().equals("pt") ? reserve.getService().name : reserve.getService().nameEN;
+        holder.nameService.setText(nameService);
+        holder.date.setText(reserve.getTime());
+
+        // Set Status
+        String reserveStatus = "";
+        switch (reserve.getStatus()) {
+            case "P":
+                reserveStatus = context.getString(R.string.pending);
+                holder.status.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#009688")));
+                break;
+            case "R":
+                reserveStatus = context.getString(R.string.rejected);
+                holder.status.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#932218")));
+                break;
+            case "A":
+                reserveStatus = context.getString(R.string.accepted);
+                holder.status.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF189329")));
+                break;
+            case "C":
+                reserveStatus = context.getString(R.string.canceled);
+                holder.status.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#b45f06")));
+                break;
+        }
+        holder.status.setText(reserveStatus);
+
+        // View Button
+        holder.reserveOpen.setOnClickListener(view -> {
+            /*if (context instanceof HomeActivity) {
+                HomeActivity homeActivity = (HomeActivity) context;
+                homeActivity.changeFragmentService(R.id.action_nav_order, order.getId(), order.getService().type);
+            }*/
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return reservesList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView nameService;
+        private final TextView date;
+        private final TextView status;
+        private final Button reserveOpen;
+        private final ImageView iconService;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameService = itemView.findViewById(R.id.txtNameService);
+            date = itemView.findViewById(R.id.txtDateReserve);
+            status = itemView.findViewById(R.id.txtStatusReserve);
+            reserveOpen = itemView.findViewById(R.id.btnReserveOpen);
+            iconService = itemView.findViewById(R.id.imageService);
+
+
+        }
+    }
+}
---
+++ happyguest-mobile/app/src/main/java/ipl/estg/happyguest/utils/models/Reserve.java
@@ -0,0 +1,65 @@
+package ipl.estg.happyguest.utils.models;
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class Reserve {
+
+    @SerializedName("id")
+    private final Long id;
+    @SerializedName("service")
+    private final Service service;
+    @SerializedName("nr_people")
+    private final Long nr_people;
+    @SerializedName("time")
+    private final String time;
+    @SerializedName("status")
+    private final String status;
+    @Nullable
+    @SerializedName("comment")
+    private final String comment;
+    @SerializedName("created_at")
+    private final String createdAt;
+
+    public Reserve(Long id, Service service, Long nr_people, String time, String status, @Nullable String comment, String createdAt) {
+        this.id = id;
+        this.service = service;
+        this.nr_people = nr_people;
+        this.time = time;
+        this.status = status;
+        this.comment = comment;
+        this.createdAt = createdAt;
+    }
+
+    public Long getNr_people() {
+        return nr_people;
+    }
+
+    @Nullable
+    public String getComment() {
+        return comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+}
